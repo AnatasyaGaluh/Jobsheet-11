c1b1c896f59332271d223bca21514145a357c943
asus <asus@Anatasya>
2018-08-14 11:03:46 +0700

    Coba method format.

--- src/CobaOperasiString/praktikum1.java
+++ src/CobaOperasiString/praktikum1.java
@@ -58,6 +58,10 @@ public class praktikum1 {
         for(int i=0; i<arrTr.length; i++)
             System.out.println(arrTr[1]);
         
+        double d = 1545454000;
+        System.out.println(String.format("Uang saya %,.4f", d));
+        System.out.println(String.format(Locale.forLanguageTag("in-ID"),"Uang saya Rp. %,.2f", d));
+        
     }
     
 }
