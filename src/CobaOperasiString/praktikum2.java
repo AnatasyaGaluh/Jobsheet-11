7e278842e73fcf6d9c4a079d3a0db1ac5e123e48
asus <asus@Anatasya>
2018-08-14 11:34:12 +0700

    Initial Commit

---
+++ src/CobaOperasiString/praktikum2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CobaOperasiString;
+
+import java.util.Scanner;
+public class praktikum2 {
+    private static void tampilJudul(String identitas){
+        System.out.println("Identitas : " + identitas);
+        System.out.println("\nCovert Kalimat Alay Angka (Vokal Ke Angka)\n");
+    }
+        public static void main(String[] args){
+        String identitas = "anatasya galuh risamita dewi salsadila / xrpl2 /05";
+        tampilJudul(identitas);
+        }
+        
+}
