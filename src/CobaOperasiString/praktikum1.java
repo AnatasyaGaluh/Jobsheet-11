af9d6772a6decc31e1afb0cf6caad9f15927dfd8
asus <asus@LAPTOP-FPE5BM76>
2018-08-14 09:06:27 +0700

    Initial Commit

---
+++ src/CobaOperasiString/praktikum1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CobaOperasiString;
+
+import java.util.Scanner;
+import java.util.Locale;
+public class praktikum1 {
+    public static void main(String[] args)
+    {
+        String identitas = ("Anatasya Galuh Risamita Dewi Salsadila / XRPL2 / 05");
+        System.out.println("Identitas : " + identitas);
+        
+    }
+}
