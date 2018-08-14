6fc9f8e9ff9919bf9836fe6fc565c7efcda79e57
asus <asus@Anatasya>
2018-08-14 09:41:20 +0700

    Coba method length & isEmpty

--- src/CobaOperasiString/praktikum1.java
+++ src/CobaOperasiString/praktikum1.java
@@ -10,8 +10,13 @@ import java.util.Locale;
 public class praktikum1 {
     public static void main(String[] args)
     {
-        String identitas = ("Anatasya Galuh Risamita Dewi Salsadila / XRPL2 / 05");
-        System.out.println("Identitas : " + identitas);
+        String x = "Operasi";
+        System.out.println("Isi variable x : " + x);
+        System.out.println("\""+ x + "\" panjang karakter = " + x.length());
+        System.out.println(" x adalah kosong : " + x.isEmpty());
+        String y = "";
+        System.out.println("isi variable y : " + y);
+        System.out.println("y adalah kosong : " + y.isEmpty());
         
     }
 }
