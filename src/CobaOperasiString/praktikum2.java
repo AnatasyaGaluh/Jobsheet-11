206c86c6bc2d220ec9a78e13d111db33b99fcdea
asus <asus@Anatasya>
2018-08-16 20:44:03 +0700

    Tambah Input

--- src/CobaOperasiString/praktikum2.java
+++ src/CobaOperasiString/praktikum2.java
@@ -15,5 +15,18 @@ public class praktikum2 {
         String identitas = "anatasya galuh risamita dewi salsadila / xrpl2 /05";
         tampilJudul(identitas);
         }
+       
+       private static String tampilInput(){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Masukkan Kalimat : ");
+            String kalimat = scanner.nextLine();
+            System.out.println("Kalimat Asli : " + kalimat);
+            return kalimat;
+        }
+
+   
+    
+        String kalimat = tampilInput();
+        
         
 }
