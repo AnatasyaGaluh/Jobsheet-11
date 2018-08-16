4149cc3e566e93130463b52491efb74896923e20
asus <asus@Anatasya>
2018-08-16 20:43:11 +0700

    Initial Commit

--- src/CobaOperasiString/praktikum2.java
+++ src/CobaOperasiString/praktikum2.java
@@ -11,6 +11,9 @@ public class praktikum2 {
         System.out.println("Identitas : " + identitas);
         System.out.println("\nCovert Kalimat Alay Angka (Vokal Ke Angka)\n");
     }
-       
+       public static void main(String[] args){
+        String identitas = "anatasya galuh risamita dewi salsadila / xrpl2 /05";
+        tampilJudul(identitas);
+        }
         
 }
